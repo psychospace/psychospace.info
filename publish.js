e9ffeb1d23ea57a79fe816090fe9c2646af7cd0e
January N. <inelsona@gmail.com>
2024-07-30 18:55:04 -0400

    Update publish.css to be in line with current ITS theme, add Git button

--- publish.js
+++ publish.js
@@ -8,3 +8,22 @@ if(Math.floor(Math.random() * 16) == 0){
     span.innerHTML = "I WAS HERE"
     footer.appendChild(span);
 }
+
+//https://discord.com/channels/947626014136221767/1223409538099576832/1261432039811448842
+//Button creation
+let button = document.createElement("button")
+button.className = "sidebar-toggle-button"; //using an existing classname to hopefully get CSS for the button
+button.setAttribute("name", "github-button")
+button.setAttribute("onclick", "gitHubButtonClick()");
+button.innerHTML = "View page on GitHub";
+let buttonContainer = document.getElementsByClassName("graph-view-outer")[0];
+buttonContainer.appendChild(button);
+
+function gitHubButtonClick(){
+    window.open(`https://github.com/psychospace/psychospace.info/blob/main${getScrubbedPath()}.md`, '_blank').focus();
+}
+
+function getScrubbedPath(){
+    let path = window.location.pathname.split('+').join(' ');
+    return path;
+}
